f561583c199d51e60573a28d28f3d65fa64d4710
Derek-w <haocheng.wang@mobileappscompany.us>
2018-05-24 08:35:47 -0400

    initial commmit

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
+    @IBAction func fbLoginPressed(_ sender: Any) {
+        let fbLoginManager: FBSDKLoginManager = FBSDKLoginManager()
+        
+        fbLoginManager.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error == nil {
+                if let fbLoginResult = result {
+                    if fbLoginResult.grantedPermissions != nil && fbLoginResult.grantedPermissions.contains("email"){
+                        self.getFBUserdata()
+                    }
+                }
+            }
+        }
+        getFBUserdata()
+    }
+    
+    func getFBUserdata(){
+        if FBSDKAccessToken.current() != nil{
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"id,name,first_name, last_name, picture.type(large), email"]).start(completionHandler: { (cnnection, result, error) in
+                if error == nil{
+                    print(result ?? "Error in getFBUserdata function")
+                }
+            })
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        if let accessToken = FBSDKAccessToken.current() {
+            print("User is logged in with access token: \(accessToken)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,26 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+inhibit_all_warnings!
+
+target 'MyApp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MyApp
+  
+  pod 'FBSDKLoginKit'
+  pod 'FacebookCore'
+  pod 'FacebookShare'  
+  
+  target 'MyAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MyAppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
